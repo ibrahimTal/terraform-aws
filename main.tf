f30b3bf8a4e7fa2871090a0120a0b98f1e00ee89
ibrahimTal <iboutal61.it@gmail.com>
2022-05-08 21:38:49 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 }*/
 
 resource "aws_instance" "premier-serveur-aws" {
-  ami           = "ami-04505e74c0741db8d"
-  instance_type = "t2.micro"
+  ami           = "ami-**********"
+  instance_type = "instance type"
 }
 
